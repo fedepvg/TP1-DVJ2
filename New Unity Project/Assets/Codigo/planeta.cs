fedc705131635c9e8c3960c0b560ac4ffe901966
fedepvg <federicopvg@gmail.com>
2019-03-25 13:00:43 -0300

    iniciado el tp

---
+++ New Unity Project/Assets/Codigo/planeta.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planeta : MonoBehaviour
+{
+    public string nombrePlaneta;
+    public float radio;
+    public Vector3 pos;
+    public GameObject sol;
+    public float angulo;
+
+    void orbitar()
+    {
+        transform.position = new Vector3(transform.position.x + radio * Mathf.Cos(angulo += Time.deltaTime),
+            transform.position.y + radio * Mathf.Sin(angulo += Time.deltaTime),
+            transform.position.z);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Planeta: " + nombrePlaneta);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        orbitar();
+    }
+}
