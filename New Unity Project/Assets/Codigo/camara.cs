85034c76613fbba5bf31a1542a7d7d4ea023d841
fedepvg <federicopvg@gmail.com>
2019-03-30 20:28:44 -0300

    agregado el trail

--- New Unity Project/Assets/Codigo/camara.cs
+++ New Unity Project/Assets/Codigo/camara.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 public class camara : MonoBehaviour
 {
     Vector3 posOriginal;
-    const int cantPlanetas = 8;
-    planeta[] planetas=new planeta[cantPlanetas];
-    
+    const int cantPlanetas = 9;
+    public GameObject[] posCamara=new GameObject[cantPlanetas];
+    int contadorPosiciones = 0;
+    float tiempoCamara;
+    const float tiempoPorPos = 3f;
+    const float distanciaCamara = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        //planetas = new planeta[cantPlanetas];   
-        posOriginal = transform.position;   
+        posOriginal = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tiempoCamara += Time.deltaTime;
+        if (tiempoCamara >= (contadorPosiciones + 1) * tiempoPorPos)
+        {
+            contadorPosiciones++;
+
+            if (contadorPosiciones == cantPlanetas)
+            {
+                tiempoCamara = 0f;
+                contadorPosiciones = 0;
+            }
+        }
+
+        switch(contadorPosiciones)
+        {
+            case 0:
+                transform.position = posOriginal;
+                break;
+            default:
+                transform.position = new Vector3 (posCamara[contadorPosiciones].transform.position.x, posCamara[contadorPosiciones].transform.position.y, 
+                                    posCamara[contadorPosiciones].transform.position.z-distanciaCamara);
+                break;
+        }
     }
 }
