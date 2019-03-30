894081d5d635d39447c2190f2ce804ea765209b8
fedepvg <federicopvg@gmail.com>
2019-03-30 18:28:24 -0300

    orbita correcta

--- New Unity Project/Assets/Codigo/planeta.cs
+++ New Unity Project/Assets/Codigo/planeta.cs
@@ -9,11 +9,13 @@ public class planeta : MonoBehaviour
     public Vector3 pos;
     public GameObject sol;
     public float angulo;
+    public float velOrbita;
+    public float velGiro;
 
     void orbitar()
     {
-        transform.position = new Vector3(transform.position.x + radio * Mathf.Cos(angulo += Time.deltaTime),
-            transform.position.y + radio * Mathf.Sin(angulo += Time.deltaTime),
+            transform.position = new Vector3(sol.transform.position.x + radio * Mathf.Cos(angulo + (velOrbita * Time.deltaTime)),
+            sol.transform.position.y + radio * Mathf.Sin(angulo + (velOrbita * Time.deltaTime)),
             transform.position.z);
     }
 
@@ -26,6 +28,11 @@ public class planeta : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        radio = Vector3.Distance(sol.transform.position, transform.position);
+
+        Vector3 targetDir =  transform.position - sol.transform.position;
+        angulo = Mathf.Atan2(targetDir.y, targetDir.x);
         orbitar();
+        
     }
 }
